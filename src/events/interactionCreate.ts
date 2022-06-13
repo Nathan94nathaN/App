0ef487206d09a66c16315670e6f0d1d6dfcc44f4
Nathan94nahtaN <nmarquesduval@gmail.com>
2022-06-13 17:10:31 +0200

    Update leaderboard command

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -193,7 +193,7 @@ export const execute: Event["execute"] = async (client: Game, interaction: Comma
                   title: "Leaderboard",
                   description: "Server Rating",
                   color: 0x00ff00,
-                  fields: ldUsers.slice(10 * (user.leaderboard.page - 1)).map((u, index) => ({
+                  fields: ldUsers.slice(10 * (user.leaderboard.page - 1)).sort((a, b) => b["level"] - b["level"] || b["xp"] - a["xp"]).map((u, index) => ({
                     name: `#${index + 10 * (user.leaderboard.page - 1) + 1}. ${interaction.guild?.members.cache.get(u["id"])?.displayName || client.users.cache.get(u["id"])?.username}`,
                     value: `🔥 Level ➜ \`${u["level"]}\`\n🎩 XP ➜ \`${kFormatter(u["xp"])}\`\n🎖 Messages ➜ \`${kFormatter(u["messages"])}\``,
                     inline: true
